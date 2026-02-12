3bd891866fbebc3e797695c302cb9139fac85714
Hamid Jaeyoung Jahangir <hamidjjahangir@gmail.com>
2026-02-11 21:13:24 -0700

    Fixed up more incompatible HTML, changed class to className, converted the login,.jsx to be the landing page

--- src/leaderboard/leaderboard.jsx
+++ src/leaderboard/leaderboard.jsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import './leaderboard.css';
 
 export function Leaderboard() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>Leaderboard displayed here</div>
+    <main className="container-fluid text-center">
+      <div className="table-responsive">
+      <table className="table table-danger table-striped-columns">
+        <thead className="table-dark">
+          <tr>
+            <th>Ranking</th>
+            <th>Players</th>
+            <th>Rounds</th>
+            <th>Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>SuperCoolKid, CoolerKid, AnotherKid</td>
+            <td>10</td>
+            <td>January 26, 2026</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>MegaCoolKid, AverageKid, UberCoolKid</td>
+            <td>8</td>
+            <td>January 26, 2026</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td>SuperDuperCoolKid, BigKid, LittleKid</td>
+            <td>7</td>
+            <td>January 25, 2026</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
     </main>
   );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,26 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main className="container-fluid text-center align-items-center">
+        <form method="get" action="game.html">
+        <h1>Ready to get your game on?</h1>
+        <div className="input-group mb-3">
+          <span className="input-group-text">Username</span>
+          <input className="form-control" type="text" placeholder="e.g. supercoolplayer" style={{maxWidth: '400px' }}/>
+        </div>
+        <div className="input-group mb-3">
+          <span className="input-group-text">Password</span>
+          <input className="form-control" type="password" placeholder="e.g 1234" />
+        </div>
+
+        <div className="form-check mb-3">
+            <input className="form-check-input" type="checkbox" id="checkbox1" name="varCheckbox" value="checkbox1"/>
+            <label className="form-check-label" htmlfor="checkbox1">Remember Username</label>
+        </div>
+        
+        <button type="submit" className="btn btn-primary">Login</button>
+        <button type="submit" className="btn btn-success">Register</button>
+      </form>
     </main>
   );
 }
