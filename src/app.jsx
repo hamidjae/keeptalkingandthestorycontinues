37860728daf34c47769324183c7dccfef8e8a51c
Hamid Jaeyoung Jahangir <hamidjjahangir@gmail.com>
2026-02-11 20:49:50 -0700

    Converted the login into React

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,30 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+    <div className="body bg-dark text-light">
+      <header className="container-fluid">
+        <nav className="navbar fixed-top navbar-dark bg-dark">
+          <a className="navbar-brand" href="#">Keep Talking...</a>
+          <menu className="navbar-nav">
+            <li className="nav-item"><a className="nav-link active" href="index.html">Home</a></li>
+            <li className="nav-item"><a className="nav-link" href="game.html">Play Game!</a></li>
+            <li className="nav-item"><a className="nav-link" href="leaderboard.html">Leaderboards</a></li>
+            <li className="nav-item"><a className="nav-link" href="about.html">About</a></li>
+          </menu>
+        </nav>
+      </header>
+
+      <main className="container-fluid text-center align-items-center">
+        App components go here
+      </main>
+
+      <footer className="bg-dark text-white-50">
+        <div className="container-fluid">
+          <span className="text-reset">Created by Hamid Jaeyoung Jahangir</span>
+          <a className="text-reset ms-2" href="https://github.com/hamidjae/keeptalkingandthestorycontinues">Github</a>
+        </div>
+      </footer>
+    </div>
+  );
 }
