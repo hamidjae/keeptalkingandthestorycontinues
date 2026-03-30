a327ac9e7e9a34b145de106ce372342fbbdf91cd
Hamid Jaeyoung Jahangir <hamidjjahangir@gmail.com>
2026-03-29 18:24:05 -0600

    Added functionality to database.js, reworded console.log to console.error to make it more standard.

--- service/database.js
+++ service/database.js
@@ -3,14 +3,56 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('startup');
+
+const userCollection = db.collection('user');
+const scoreCollection = db.collection('score');
 
 (async function testConnection() {
   try {
     await db.command({ ping: 1 });
     console.log('Connected to database');
   } catch (ex) {
-    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    console.error(`Unable to connect to database because ${ex.message}`);
     process.exit(1);
   }
 })();
+
+async function getUser(email) {
+  return userCollection.findOne({ email });
+}
+
+async function getUserByToken(token) {
+  return userCollection.findOne({ token });
+}
+
+async function createUser(user) {
+  await userCollection.insertOne(user);
+  return user;
+}
+
+async function updateUserToken(email, token) {
+  await userCollection.updateOne({ email }, { $set: { token } });
+}
+
+async function clearUserToken(token) {
+  await userCollection.updateOne({ token }, { $unset: { token: "" } });
+}
+
+async function addScore(score) {
+  await scoreCollection.insertOne(score);
+}
+
+async function getHighScores() {
+  return scoreCollection.find().sort({ score: -1 }).limit(10).toArray();
+}
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  createUser,
+  updateUserToken,
+  clearUserToken,
+  addScore,
+  getHighScores,
+};
